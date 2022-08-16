f8731df49358d639f260cdf834b5bee49ad70be8
Artem Aseev <artem.aseev@gmail.com>
2022-08-16 19:01:36 +0300

    add README.md, Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SalesManager SalesManager = new SalesManager(new int[]{2, 3, 3, 14, 10});
+
+        System.out.println(SalesManager.max());
+    }
+}
